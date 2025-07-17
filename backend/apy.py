4d39650723cb7d1c7c21f72c53dd9f42b502f0a0
lboutier <laurine.boutier@isen-ouest.yncrea.fr>
2025-07-17 10:37:25 +0200

    Update apy.py

--- backend/apy.py
+++ backend/apy.py
@@ -46,6 +46,40 @@ def get_admissions():
 @app.route("/")
 def home():
     return "✔️ Flask API is running!"
+@app.route("/get_hospitals", methods=["GET"])
+def get_hospitals():
+    hospitals_data = [
+        {
+            "Nom": "Rotthalmünster",
+            "Specialites": ["Stroke", "Anesthesia", "Cardiology"],
+            "Latitude": 48.354987,
+            "Longitude": 13.201969,
+            "Lits_disponibles": 12,
+            "Capacite": 100
+        },
+        {
+            "Nom": "Vilshofen",
+            "Specialites": ["Geriatrics", "Cardiology", "Pediatrics"],
+            "Latitude": 48.627808,
+            "Longitude": 13.19838,
+            "Lits_disponibles": 5,
+            "Capacite": 150
+        },
+        {
+            "Nom": "Wegscheid",
+            "Specialites": ["Gynecology", "Radiology", "Orthopedics"],
+            "Latitude": 48.6012951,
+            "Longitude": 13.7930649,
+            "Lits_disponibles": 20,
+            "Capacite": 120
+        }
+    ]
+
+    # Calculer le taux d’occupation pour chaque hôpital
+    for h in hospitals_data:
+        h["Taux_occupation"] = round(((h["Capacite"] - h["Lits_disponibles"]) / h["Capacite"]) * 100, 2)
+
+    return jsonify(hospitals_data)
 
 if __name__ == "__main__":
     init_db()
